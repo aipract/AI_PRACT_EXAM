834eacaef5dc80493d60d4586dfb9e20d8e37fb6
aipract <116038262+aipract@users.noreply.github.com>
2022-10-18 03:03:50 +0530

    Add files via upload

---
+++ HanoiPole.py
@@ -0,0 +1,9 @@
+#AIM - Tower of Hanoi Problem
+def moveTower(height, fromPole, toPole, withPole):
+    if height>=1:
+        moveTower(height-1, fromPole, withPole, toPole)
+        moveDisk(fromPole, toPole)
+        moveTower(height-1, withPole, toPole, fromPole)
+def moveDisk(fp,tp):
+    print("moving disk from",fp,"to",tp)
+moveTower(3,"A","B","C")
